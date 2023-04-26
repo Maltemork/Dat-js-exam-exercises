5e370da552f10c6fda5b079530f9c2dd35dc10e3
Maltemork <maltems1996@gmail.com>
2023-04-26 13:59:31 +0200

    fixes

--- environments/environment-01/script.js
+++ environments/environment-01/script.js
@@ -1,2 +1,29 @@
 "use strict";
+window.addEventListener("load", start);
 
+async function start() {
+  const userData = await loadJsonFile("users.json");
+  console.log(userData);
+  showUser(userData);
+}
+
+async function loadJsonFile(url) {
+  const users = await fetch(url);
+  const data = await users.json();
+  return data;
+}
+
+function showUser(userList) {
+  for (const user of userList) {
+    const userHTML = /*HTML*/ `
+    
+        <li>Name: ${user.name} Active?: ${user.active}</li>
+    
+    
+    `;
+
+    document
+      .querySelector("#userlist")
+      .insertAdjacentHTML("beforeend", userHTML);
+  }
+}
