4438141cce1b9a02ee3c0b8f2b55d39a2ea1ed6a
Maltemork <malte.sorensen@protonmail.com>
2023-05-31 20:26:13 +0200

    done

--- environments/environment-05/script.js
+++ environments/environment-05/script.js
@@ -1,1 +1,61 @@
 "use strict";
+
+import { courses } from "./courses.js";
+
+window.addEventListener("load", start);
+
+function start() {
+  showCourses();
+  createNewCourse(
+    "Python introductory Course",
+    "2024-01-01",
+    "2024-06-30",
+    10,
+    30,
+    "Peter"
+  );
+}
+
+function showCourses() {
+  document.querySelector("#courses-list").innerHTML = "";
+  courses.forEach(showInHTML);
+
+  function showInHTML(course) {
+    let HTMLelement = /*HTML*/ `
+    <article>
+      <li>Course: ${course.name}</li>
+      <li>Starts: ${course.startDate}</li>
+      <li>Ends: ${course.endDate}</li>
+      <li>ECTS Points: ${course.ectsPoints}</li>
+      <li>Student capacity: ${course.maxStudents}</li>
+      <li>Teacher: ${course.teacher}</li>
+      <br>
+    </article>
+    `;
+
+    document
+      .querySelector("#courses-list")
+      .insertAdjacentHTML("beforeend", HTMLelement);
+  }
+}
+
+function createNewCourse(
+  nameParm,
+  startDateParm,
+  endDateParm,
+  ectsPointsParm,
+  maxStudentsParm,
+  teacherParm
+) {
+  let newCourse = {
+    name: `${nameParm}`,
+    startDate: `${startDateParm}`,
+    endDate: `${endDateParm}`,
+    ectsPoints: ectsPointsParm,
+    maxStudents: maxStudentsParm,
+    teacher: `${teacherParm}`,
+  };
+
+  courses.push(newCourse);
+  showCourses();
+}
