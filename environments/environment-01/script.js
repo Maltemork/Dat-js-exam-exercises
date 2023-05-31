3a9be60f12dfb611b75a72e76cd748cc17fe010a
Maltemork <maltems1996@gmail.com>
2023-05-31 13:22:51 +0200

    done

--- environments/environment-01/script.js
+++ environments/environment-01/script.js
@@ -1,2 +1,50 @@
 "use strict";
 
+window.addEventListener("load", start);
+
+const users = [];
+
+async function start() {
+  await getData();
+  users.forEach(showInHTML);
+}
+
+async function getData() {
+  const response = await fetch("users.json");
+  const data = await response.json();
+
+  for (const key in data) {
+    users.push(data[key]);
+  }
+}
+
+function showInHTML(user) {
+  let HTMLobject =
+    /*HTML*/
+    `
+  <article>
+    <li>${user.name}</li>
+    <li>${user.active}</li>
+    <li>${user.role}</li>
+    <br>
+  </article>
+  `;
+
+  if (user.active === true) {
+    document
+      .querySelector("#userlist")
+      .insertAdjacentHTML("beforeend", HTMLobject);
+  }
+}
+
+function addUser(nameParm, activeParm, roleParm) {
+  let user = {
+    name: nameParm,
+    active: activeParm,
+    role: roleParm,
+  };
+
+  document.querySelector("#userlist").innerHTML = "";
+  users.push(user);
+  users.forEach(showInHTML);
+}
