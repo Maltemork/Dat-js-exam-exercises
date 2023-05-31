5bc2416d8c139826790febf070415cb23854df03
Maltemork <maltems1996@gmail.com>
2023-05-31 11:40:31 +0200

    done

--- environments/environment-00/script.js
+++ environments/environment-00/script.js
@@ -1,1 +1,17 @@
 "use strict";
+
+window.addEventListener("load", start);
+
+function start() {
+  const resultFailure = document.querySelector("#result_failure");
+  const resultSuccess = document.querySelector("#result_success");
+
+  resultFailure.classList.add("hide");
+  resultSuccess.classList.add("hide");
+
+  document.querySelector("#btn-knap").addEventListener("click", () => {
+    console.log("Det virker.");
+    resultSuccess.classList.add("show");
+    resultFailure.classList.add("hide");
+  });
+}
