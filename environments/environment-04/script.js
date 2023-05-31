9cadc20bed885bc7bbff902d8c214ade14e07495
Maltemork <malte.sorensen@protonmail.com>
2023-05-31 18:54:57 +0200

    done

--- environments/environment-04/script.js
+++ environments/environment-04/script.js
@@ -1,1 +1,30 @@
 "use strict";
+
+import { teachers } from "./teachers.js";
+
+window.addEventListener("load", start);
+
+function start() {
+  showTeachers();
+}
+
+function showTeachers() {
+  let ByName = (a, b) => a.name.localeCompare(b.name);
+  let ByEmail = (a, b) => a.email.localeCompare(b.email);
+
+  teachers.sort(ByEmail);
+
+  teachers.forEach(insertHTML);
+
+  function insertHTML(teacher) {
+    let HTMLelement = /*HTML*/ `
+          <li>${teacher.name}</li>
+          <li>${teacher.email}</li>
+          <br>
+        `;
+
+    document
+      .querySelector("#teachers-list")
+      .insertAdjacentHTML("beforeend", HTMLelement);
+  }
+}
