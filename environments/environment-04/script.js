5a1bdc48871abd6aaa6087df741f8e7317b77e52
Maltemork <malte.sorensen@protonmail.com>
2023-05-31 19:05:42 +0200

    done

--- environments/environment-04/script.js
+++ environments/environment-04/script.js
@@ -1,1 +1,38 @@
 "use strict";
+
+import { teachers } from "./teachers.js";
+
+window.addEventListener("load", start);
+
+function start() {
+  showTeachers();
+  createTeacher("Malte", "maltems1996@gmail.com");
+}
+
+function showTeachers() {
+  document.querySelector("#teachers-list").innerHTML = "";
+  teachers.forEach(insertHTML);
+
+  function insertHTML(teacher) {
+    let HTMLelement = /*HTML*/ `
+      <article>
+        <li>${teacher.name}</li>
+        <li>${teacher.email}</li>
+        <br>
+      </article>
+    `;
+
+    document
+      .querySelector("#teachers-list")
+      .insertAdjacentHTML("beforeend", HTMLelement);
+  }
+}
+
+function createTeacher(nameParm, emailParm) {
+  let newTeacher = {
+    name: nameParm,
+    email: emailParm,
+  };
+  teachers.push(newTeacher);
+  showTeachers();
+}
